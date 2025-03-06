6146405472442c5253d984a252bdc10c2b678e07
Raihan260 <152713313+Raihan260@users.noreply.github.com>
2025-03-06 14:33:30 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'Home_Page/home_page.dart';
+import 'bottom_nav_bar.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "SDG Adventure",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(),
+      home: BottomNavScreen(),
     );
   }
 }
-
