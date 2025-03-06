f4936d23b6d54737cfc59e7840838371a20640e7
Raihan260 <152713313+Raihan260@users.noreply.github.com>
2025-03-06 14:34:32 +0700

    Add files via upload

---
+++ lib/bottom_nav_bar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'Home_Page/home_page.dart';
+import 'Search_Page/search_page.dart';
+import 'Profile_Page/profile_page.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  @override
+  _BottomNavScreenState createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    HomePage(),      // Halaman Home
+    SearchPage(),    // Halaman Search
+    ProfilePage(),   // Halaman Profile
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.yellow,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          selectedItemColor: Colors.blue.shade900,
+          unselectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
